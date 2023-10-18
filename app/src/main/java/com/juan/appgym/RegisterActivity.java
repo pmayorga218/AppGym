f0cf58a41a72ff54709d59c102ed40ce3a292f87
just <juanfelipecabanzo041@gmail.com>
2023-10-18 16:34:14 -0500

    Initial commit

---
+++ app/src/main/java/com/juan/appgym/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.juan.appgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+    public void irlogin(View view){
+        finish();
+
+    }
+}
