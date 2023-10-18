2ec7327823f976784aedb4cb6244b8cef9d0a77f
just <juanfelipecabanzo041@gmail.com>
2023-10-18 17:35:16 -0500

    adelanto 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //implementacion de los colores
+    implementation("com.github.yukuku:ambilwarna:2.0.1")
+    //implementacion para la imagen redodna
+    implementation("de.hdodenhof:circleimageview:3.0.1")
+    //Picasso para poder mostrar imagenes de internet
+    implementation("com.squareup.picasso:picasso:2.8")
+    //esta es la implementacion para lottie splah screen
+    implementation ("com.airbnb.android:lottie:6.1.0")
+
 }
