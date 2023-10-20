8dac51e4153066b797b93b8030fe6a73b382b2b3
cisco <Nrodriguez895@unab.edu.co>
2023-10-20 17:48:39 -0500

    avance nicolas

---
+++ app/src/main/java/com/juan/appgym/ObjetivoActivity.java
@@ -0,0 +1,14 @@
+package com.juan.appgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObjetivoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objetivo);
+    }
+}
