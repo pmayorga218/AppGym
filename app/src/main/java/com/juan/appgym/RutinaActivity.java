fa7ab0f9fa97e327d7c237a0f24567d21c34ced0
just <juanfelipecabanzo041@gmail.com>
2023-10-20 17:42:56 -0500

    adelanto c2

--- app/src/main/java/com/juan/appgym/RutinaActivity.java
+++ app/src/main/java/com/juan/appgym/RutinaActivity.java
@@ -2,16 +2,13 @@ package com.juan.appgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class InicioActivity extends AppCompatActivity {
+public class RutinaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicio);
+        setContentView(R.layout.activity_rutina);
     }
-
 }
