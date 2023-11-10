ed29125cdeaae955f96a337ea274872184c73a0a
Hogar <nrodriguez895@unab.edu.co>
2023-11-10 02:41:13 -0500

    esto solo tiene strnigs de mi parte

--- app/src/main/java/com/juan/appgym/ActividadActivity.java
+++ app/src/main/java/com/juan/appgym/ActividadActivity.java
@@ -1,5 +1,4 @@
 package com.juan.appgym;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,41 +11,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ActividadActivity extends AppCompatActivity {
 
-    private static final String PREFS_NAME = "MyPrefsFile";
-    private static final String CHECKBOX1_KEY = "CheckBox1";
-    private static final String CHECKBOX2_KEY = "CheckBox2";
-    private static final String CHECKBOX3_KEY = "CheckBox3";
-    private static final String CHECKBOX4_KEY = "CheckBox4";
-    private static final String CHECKBOX5_KEY = "CheckBox5";
+    private static final String CHECKBOX_PREFS = "CheckBoxPrefs";
+    private static final String CHECKBOX_KEYS[] = {"CheckBox1", "CheckBox2", "CheckBox3", "CheckBox4", "CheckBox5"};
 
-    private CheckBox checkBox1, checkBox2, checkBox3, checkBox4, checkBox5;
+    private CheckBox checkBox[] = new CheckBox[CHECKBOX_KEYS.length];
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad);
 
-        checkBox1 = findViewById(R.id.checkBox1);
-        checkBox2 = findViewById(R.id.checkBox2);
-        checkBox3 = findViewById(R.id.checkBox3);
-        checkBox4 = findViewById(R.id.checkBox4);
-        checkBox5 = findViewById(R.id.checkBox5);
-
-        // Lee el estado de los CheckBox y marca los CheckBox correspondientes
-        restoreCheckBoxState(checkBox1, CHECKBOX1_KEY);
-        restoreCheckBoxState(checkBox2, CHECKBOX2_KEY);
-        restoreCheckBoxState(checkBox3, CHECKBOX3_KEY);
-        restoreCheckBoxState(checkBox4, CHECKBOX4_KEY);
-        restoreCheckBoxState(checkBox5, CHECKBOX5_KEY);
+        for (int i = 0; i < CHECKBOX_KEYS.length; i++) {
+            checkBox[i] = findViewById(getResources().getIdentifier("checkBox" + (i + 1), "id", getPackageName()));
+            restoreCheckBoxState(checkBox[i], CHECKBOX_KEYS[i]);
+        }
     }
 
     public void irperfilc(View view) {
         // Guarda el estado de los CheckBox al hacer clic en "Confirmar"
-        saveCheckBoxState(checkBox1, CHECKBOX1_KEY);
-        saveCheckBoxState(checkBox2, CHECKBOX2_KEY);
-        saveCheckBoxState(checkBox3, CHECKBOX3_KEY);
-        saveCheckBoxState(checkBox4, CHECKBOX4_KEY);
-        saveCheckBoxState(checkBox5, CHECKBOX5_KEY);
+        for (int i = 0; i < CHECKBOX_KEYS.length; i++) {
+            saveCheckBoxState(checkBox[i], CHECKBOX_KEYS[i]);
+        }
 
         // Vuelve al xml de perfil
         Intent intent = new Intent(this, PerfilActivity.class);
@@ -54,16 +39,17 @@ public class ActividadActivity extends AppCompatActivity {
     }
 
     private void saveCheckBoxState(CheckBox checkBox, String key) {
-        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).edit();
+        SharedPreferences.Editor editor = getSharedPreferences(CHECKBOX_PREFS, Context.MODE_PRIVATE).edit();
         editor.putBoolean(key, checkBox.isChecked());
         editor.apply();
     }
 
     private void restoreCheckBoxState(CheckBox checkBox, String key) {
-        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences prefs = getSharedPreferences(CHECKBOX_PREFS, Context.MODE_PRIVATE);
         checkBox.setChecked(prefs.getBoolean(key, false));
     }
 }
+
 
 
 
